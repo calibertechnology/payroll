0173bb8f0e3e8e1b2d9436733770500b51cc9b23
wayne <wrichards@taxupdate.com>
2022-02-25 06:37:03 -0600

    Renames employee_taxes, adds status, exemptions, misc and aux.

--- server/create.sql
+++ server/create.sql
@@ -22,22 +22,26 @@ CREATE TABLE employees (
   zip VARCHAR(255),
   PRIMARY KEY(id)
 );
-DROP TABLE IF EXISTS employee_tax_names;
-CREATE TABLE employee_tax_names (
+DROP TABLE IF EXISTS employee_taxes;
+CREATE TABLE employee_taxes (
   emp_id INT NOT NULL REFERENCES employees(id),
   taxname VARCHAR(255) NOT NULL,
+  filing_status INT NOT NULL,
+  exemptions NUMERIC,
+  miscellaneous FLOAT,
+  auxiliary FLOAT,
   PRIMARY KEY(emp_id,taxname)
 );
 
 INSERT INTO employers (er_id, company, address, city, state, zip)
   VALUES(1, 'National Park Service', '520 Chestnut St', 'Philadelphia', 'PA', '19106');
 INSERT INTO employees (id, employer, last, first, address, city, state, zip)
-  VALUES(1, 1, 'Schmoe', 'Joe', '123 Main St', 'Aliquippa', 'PA', '15001');
-INSERT INTO employee_tax_names VALUES (1,'Federal Income Tax');
-INSERT INTO employee_tax_names VALUES (1,'FUTA With Credit Reduction');
-INSERT INTO employee_tax_names VALUES (1,'FICA SS');
-INSERT INTO employee_tax_names VALUES (1,'FICA Med');
-INSERT INTO employee_tax_names VALUES (1,'PA');
-INSERT INTO employee_tax_names VALUES (1,'PA Unemployment');
-INSERT INTO employee_tax_names VALUES (1,'PA Unemployment Employee');
-INSERT INTO employee_tax_names VALUES (1,'PA Philadelphia EIT');
+  VALUES(1, 1, 'Poe', 'Edgar', '532 N 7th St', 'Philadelphia', 'PA', '19123');
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'Federal Income Tax', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'FUTA With Credit Reduction', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'FICA SS', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'FICA Med', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'PA', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'PA Unemployment', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'PA Unemployment Employee', 0);
+INSERT INTO employee_taxes (emp_id, taxname, filing_status) VALUES (1,'PA Philadelphia EIT', 0);
